6d5105eaf58e8614941c87b9f186cb8582b94e2e
zaxtrazh <45304150+zaxtrazh@users.noreply.github.com>
2020-02-25 20:04:22 +0700

    Add files via upload

---
+++ 1_struktur_dasar.cpp
@@ -0,0 +1,20 @@
+/*
+  Name       : Struktur dasar program C++
+  Author     : M.N.Muzaki
+  Date       : 18/02/20 16:55
+  Description: Komponen dasar agar program dapat berjalan
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 	 
+    system("pause");
+    return 0;  
+}
+
+
+
